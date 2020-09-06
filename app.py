3ad4d0f11e9429e52143f730534a0852b31ee214
ioanaMMT <ioana2morosanu@gmail.com>
2020-09-06 18:21:19 +0100

    adding frames

--- app.py
+++ app.py
@@ -5,20 +5,27 @@ root = tk.Tk()
 
 user_name = tk.StringVar()
 
-name_label = ttk.Label(root, text = "User Name: ")
+input_frame = ttk.Frame(root, padding = (20, 10, 20, 0))
+input_frame.pack(fill = "both")
+
+name_label = ttk.Label(input_frame, text = "User Name: ")
 name_label.pack(side = "left")
-name_entry = ttk.Entry(root, width = 15, textvariable = user_name)
+name_entry = ttk.Entry(input_frame, width = 15, textvariable = user_name)
 name_entry.pack(side = "left")
 name_entry.focus()
+
+buttons = ttk.Frame(root, padding = (20, 10))
+buttons.pack(fill = "both")
+
 
 def to_print():
 	print(f"Hello, {user_name.get() or 'World'}")
 
-green_button = ttk.Button(root, text = "Print", command = to_print)
-green_button.pack(side = "left")
+green_button = ttk.Button(buttons, text = "Print", command = to_print)
+green_button.pack(side = "left", fill = "x", expand = True)
 
-quit_button = ttk.Button(root, text = "Quit", command = root.destroy)
-quit_button.pack(side = "right")
+quit_button = ttk.Button(buttons, text = "Quit", command = root.destroy)
+quit_button.pack(side = "right", fill = "x", expand = True)
 
 
 root.mainloop()
