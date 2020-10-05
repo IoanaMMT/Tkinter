596a2b82a4493a991b0a4f2bf3f700e1730b60a0
ioanaMMT <ioana2morosanu@gmail.com>
2020-10-05 19:03:04 +0100

    tricks in tk

--- tricksttk.py
+++ tricksttk.py
@@ -7,10 +7,26 @@ root.geometry("600x400")
 root.resizable(False, False)
 root.title("Widget Examples")
 
-selected_weekday = tk.StringVar()
+programming_languages = ("C", "Go", "JavaScript", "Perl", "Python", "Rust")
+
+langs = tk.StringVar(value = programming_languages)
+langs_select = tk.Listbox(root, listvariable = langs, height = 6)
+langs_select["selectmode"] = "extended" #browse
+langs_select.pack()
+
+def handle_selection_change(event):
+	selected_indices = langs_select.curselection()
+	for i in selected_indices:
+		print(langs_select.get(i))
+
+langs_select.bind("<<ListboxSelect>>", handle_selection_change)
+
+root.mainloop()
+####### Combobox ########
+'''selected_weekday = tk.StringVar()
 weekday = ttk.Combobox(root, textvariable = selected_weekday)
 weekday["values"] = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Friday")
-weekday["state"] = "readonly"
+weekday["state"] = "readonly" #normal
 weekday.pack()
 
 def handle_selection(event):
@@ -24,3 +40,4 @@ weekday.bind("<<ComboboxSelected>>", handle_selection)
 root.mainloop()
 
 print(selected_weekday.get(), "was selected.")
+'''
